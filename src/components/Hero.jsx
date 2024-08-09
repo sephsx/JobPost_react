418d390372002d44a40acb3f1df8f119056e4af0
sephsx <olorbidajoseph05@gmail.com>
2024-08-09 21:53:58 +0800

    added card components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const Hero = () => {
+const Hero = ({title="This is Title", subtitle="This is Subtitle"}) => {
   return (
     <>
-      <section className="bg-indigo-700 py-20 mb-4">
+      <section className="bg-indigo-600 py-20 mb-4">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-6 flex flex-col items-center">
           <div className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
-           <h1>Become a React Dev</h1>
-           <p>Find the React Job that fits your skills and needs</p>
+           <h1 className='text-center text-white'>{title}</h1>
+           <p className='text-center text-2xl text-white'>{subtitle}</p>
+
           </div>
         </div>
       </section>
