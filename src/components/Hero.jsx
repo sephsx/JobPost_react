69b402fe7428132e0c420afb22eef5dee6864fec
sephsx <olorbidajoseph05@gmail.com>
2024-08-09 20:28:56 +0800

    Added Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+      <section className="bg-indigo-700 py-20 mb-4">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-6 flex flex-col items-center">
+          <div className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+           <h1>Become a React Dev</h1>
+           <p>Find the React Job that fits your skills and needs</p>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Hero
