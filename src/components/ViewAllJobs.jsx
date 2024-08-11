17a0e0e0134f53791b6c16899400746500d00877
sephsx <olorbidajoseph05@gmail.com>
2024-08-11 22:41:13 +0800

    added react-dom router and icons and readmore/less

--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ViewAllJobs = () => {
     return (
-        <section class="m-auto max-w-lg my-10 px-6">
+        <section className="m-auto max-w-lg my-10 px-6">
             <a
                 href="/jobs"
-                class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+                className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
             >View All Jobs</a>
         </section>
     )
