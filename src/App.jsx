91735e334f29d26a8fe92729ca6ff90a4d0ae187
sephsx <olorbidajoseph05@gmail.com>
2024-08-11 14:57:16 +0800

    Added VieAllJobs component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
-      <HomeCards/>
-      <JobListings/>
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
     </>
   )
 }
